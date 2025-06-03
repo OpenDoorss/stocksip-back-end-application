9dedf206b3db9eca3377e0aea1fe8149f0488dca
equinox-1092 <sdiaz1234di@gmail.com>
2025-06-03 12:22:37 -0500

    chore: initial commit.

---
+++ src/main/java/com/stocksip/StocksipBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.stocksip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksipBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StocksipBackEndApplication.class, args);
+    }
+
+}
