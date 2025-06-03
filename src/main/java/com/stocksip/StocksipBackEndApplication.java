b516c69034ab2bd25c5de399a7516cbd0bd43103
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-03 12:43:11 -0500

    chore: enable jpa auditing.

--- src/main/java/com/stocksip/StocksipBackEndApplication.java
+++ src/main/java/com/stocksip/StocksipBackEndApplication.java
@@ -2,7 +2,19 @@ package com.stocksip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+/**
+ * Stocksip Back End Application - Stocksip Platform
+ *
+ * @summary
+ * This is the main class for the Stocksip Platform application.
+ * It is responsible for bootstrapping the application and configuring the necessary components.
+ * It enables JPA auditing and starts the Spring Boot application.
+ *
+ * @since 1.0
+ */
+@EnableJpaAuditing
 @SpringBootApplication
 public class StocksipBackEndApplication {
 
