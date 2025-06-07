2b9b0f0aa90ab04ac5f6d9723f70170f95c94044
Far14z <faridce14@gmail.com>
2025-06-07 03:34:23 -0500

    feat(warehouses): add warehouse command service implementation and create warehouse command with a query validation.

---
+++ src/main/java/com/stocksip/inventorymanagement/application/internal/commandservices/WarehousesCommandServiceImpl.java
@@ -0,0 +1,46 @@
+package com.stocksip.inventorymanagement.application.internal.commandservices;
+
+import com.stocksip.inventorymanagement.domain.model.aggregates.Warehouse;
+import com.stocksip.inventorymanagement.domain.model.commands.CreateWarehouseCommand;
+import com.stocksip.inventorymanagement.domain.services.WarehousesCommandService;
+import com.stocksip.inventorymanagement.infrastructure.persistence.jpa.WarehouseRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/**
+ * @name WarehousesCommandServiceImpl
+ *
+ * @summary
+ * Implementation of the WarehousesCommandService interface for handling commands related to warehouses.
+ *
+ * @since 1.0.0
+ */
+@Service
+public class WarehousesCommandServiceImpl implements WarehousesCommandService {
+
+    /**
+     * Repository for accessing warehouse data.
+     */
+    private final WarehouseRepository warehouseRepository;
+
+    public WarehousesCommandServiceImpl(WarehouseRepository warehouseRepository) {
+        this.warehouseRepository = warehouseRepository;
+    }
+
+    /**
+     * Handles the command to create a new warehouse.
+     *
+     * @param command the command containing the details of the warehouse to be created
+     * @return an Optional containing the created Warehouse if successful, or empty if not
+     * @throws IllegalArgumentException if a warehouse with the same address already exists
+     */
+    @Override
+    public Optional<Warehouse> handle(CreateWarehouseCommand command) {
+        if (warehouseRepository.existByAddressByStreetAndCityAndPostalCode(command.address().street(), command.address().city(), command.address().postalCode()).isPresent())
+            throw new IllegalArgumentException("Warehouse with the same address already exists.");
+        var warehouse = new Warehouse(command);
+        var createdWarehouse = warehouseRepository.save(warehouse);
+        return Optional.of(createdWarehouse);
+    }
+}
