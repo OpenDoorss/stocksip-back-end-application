5754802f1de3f30e8e9123098352cefd655974b7
Far14z <faridce14@gmail.com>
2025-06-07 03:33:20 -0500

    feat(warehouses): add warehouse command service.

---
+++ src/main/java/com/stocksip/inventorymanagement/domain/services/WarehousesCommandService.java
@@ -0,0 +1,26 @@
+package com.stocksip.inventorymanagement.domain.services;
+
+import com.stocksip.inventorymanagement.domain.model.aggregates.Warehouse;
+import com.stocksip.inventorymanagement.domain.model.commands.CreateWarehouseCommand;
+
+import java.util.Optional;
+
+/**
+ * @name WarehousesCommandService
+ *
+ * @summary
+ * Service interface for handling commands related to warehouses.
+ *
+ * @since 1.0.0
+ */
+public interface WarehousesCommandService {
+
+    /**
+     * Handles the creation of a new warehouse.
+     *
+     * @param command the command containing the details for creating a warehouse
+     * @return an Optional containing the created Warehouse if successful, or empty if not
+     * @see CreateWarehouseCommand
+     */
+    Optional<Warehouse> handle(CreateWarehouseCommand command);
+}
