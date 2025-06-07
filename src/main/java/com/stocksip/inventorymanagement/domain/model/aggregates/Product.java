41bcf6981718266d095ac4992155a37c91f29599
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-06 23:40:22 -0500

    feat(products): add product aggregate root.

--- src/main/java/com/stocksip/inventorymanagement/domain/model/aggregates/Product.java
+++ src/main/java/com/stocksip/inventorymanagement/domain/model/aggregates/Product.java
@@ -1,8 +1,13 @@
 package com.stocksip.inventorymanagement.domain.model.aggregates;
 
+import com.stocksip.inventorymanagement.domain.model.valueobjects.CatalogId;
+import com.stocksip.inventorymanagement.domain.model.valueobjects.LiquorType;
+import com.stocksip.inventorymanagement.domain.model.valueobjects.ProductState;
+import com.stocksip.inventorymanagement.domain.model.valueobjects.ProviderId;
+import com.stocksip.shared.domain.aggregates.AuditableAbstractAggregateRoot;
 import jakarta.persistence.*;
 import lombok.Getter;
-import org.springframework.data.domain.AbstractAggregateRoot;
+import lombok.Setter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 /**
@@ -16,11 +21,126 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
  */
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-public class Product extends AbstractAggregateRoot<Product> {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class Product extends AuditableAbstractAggregateRoot<Product> {
+
     @Getter
-    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "warehouse_id")
+    private Warehouse warehouse;
 
+    @Getter
+    @Embedded
+    private ProviderId providerId;
 
+    @Getter
+    @Embedded
+    private CatalogId catalogId;
+
+    @Setter
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private LiquorType productType;
+
+    @Column(nullable = false)
+    private ProductState productState;
+
+    @Column(nullable = false)
+    private double unitPrice;
+
+    @Column(nullable = false)
+    private double productContent;
+
+    @Column(nullable = false)
+    private int currentStock;
+
+    @Column(nullable = false)
+    private int minimumStock;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    protected Product() {
+        // Default constructor for JPA.
+    }
+
+    public Product(Warehouse warehouse, ProviderId providerId, String imageUrl,
+                   String name, LiquorType productType, double unitPrice,
+                   double productContent, int currentStock, int minimumStock) {
+
+        this.warehouse = warehouse;
+        this.providerId = providerId;
+        this.imageUrl = imageUrl;
+        this.name = name;
+        this.productType = productType;
+        this.unitPrice = unitPrice;
+        this.productContent = productContent;
+        this.currentStock = currentStock;
+        this.minimumStock = minimumStock;
+        this.productState = ProductState.IN_WAREHOUSE;
+        // this.addDomainEvent(new ProductRequestedEvent(this));
+    }
+
+    public Product(Warehouse warehouse, String imageUrl,
+                   String name, LiquorType productType, double unitPrice,
+                   double productContent, int currentStock, int minimumStock) {
+
+        this.warehouse = warehouse;
+        this.providerId = null;
+        this.catalogId = null;
+        this.imageUrl = imageUrl;
+        this.name = name;
+        this.productType = productType;
+        this.unitPrice = unitPrice;
+        this.productContent = productContent;
+        this.currentStock = currentStock;
+        this.minimumStock = minimumStock;
+        this.productState = ProductState.IN_WAREHOUSE;
+        // this.addDomainEvent(new ProductRequestedEvent(this));
+    }
+
+    public void loss() {
+        this.productState = ProductState.LOST;
+        // this.addDomainEvent(new ProductConfirmedEvent(this));
+    }
+
+    public void sell() {
+        this.productState = ProductState.SOLD;
+        // this.addDomainEvent(new ProductConfirmedEvent(this));
+    }
+
+    public void donate() {
+        this.productState = ProductState.DONATED;
+        // this.addDomainEvent(new ProductConfirmedEvent(this));
+    }
+
+    public void consume() {
+        this.productState = ProductState.CONSUMED;
+        // this.addDomainEvent(new ProductConfirmedEvent(this));
+    }
+
+    public String getStatus() {
+        return this.productState.name().toLowerCase();
+    }
+
+    public boolean isLost() {
+        return ProductState.LOST.equals(this.productState);
+    }
+
+    public boolean isSold() {
+        return  ProductState.SOLD.equals(this.productState);
+    }
+
+    public boolean isConsumed() {
+        return ProductState.CONSUMED.equals(this.productState);
+    }
+
+    public boolean isDonated() {
+        return ProductState.DONATED.equals(this.productState);
+    }
+
+    public boolean isInWarehouse() {
+        return ProductState.IN_WAREHOUSE.equals(this.productState);
+    }
 }
--- src/main/java/com/stocksip/inventorymanagement/domain/model/valueobjects/ProductState.java
+++ src/main/java/com/stocksip/inventorymanagement/domain/model/valueobjects/ProductState.java
@@ -7,7 +7,7 @@ package com.stocksip.inventorymanagement.domain.model.valueobjects;
  * This enum defines the possible states of liquor. It is used to track the reason of the exit of a product.
  * The possible states  are:
  * - IN_WAREHOUSE: Default state for the product. It's when the product is still in the warehouse of the owner of the liquor store.
- * - BOUGHT: When the product was bought.
+ * - SOLD: When the product was sold.
  * - DONATED: When the product was donated.
  * - LOST: When the product is lost because of the expiration date has passed.
  * - CONSUMED: When a product is consumed internally by the owner of the liquor store.
@@ -16,7 +16,7 @@ package com.stocksip.inventorymanagement.domain.model.valueobjects;
  */
 public enum ProductState {
     IN_WAREHOUSE,
-    BOUGHT,
+    SOLD,
     DONATED,
     LOST,
     CONSUMED
