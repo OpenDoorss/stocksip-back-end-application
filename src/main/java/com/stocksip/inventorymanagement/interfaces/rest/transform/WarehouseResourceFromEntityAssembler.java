9acd5b9bf68b08e375ec5cc3c5de9d2f3510148a
Far14z <faridce14@gmail.com>
2025-06-09 01:28:44 -0500

    feat(warehouses): add warehouse resource from entity assembler.

---
+++ src/main/java/com/stocksip/inventorymanagement/interfaces/rest/transform/WarehouseResourceFromEntityAssembler.java
@@ -0,0 +1,25 @@
+package com.stocksip.inventorymanagement.interfaces.rest.transform;
+
+import com.stocksip.inventorymanagement.domain.model.aggregates.Warehouse;
+import com.stocksip.inventorymanagement.interfaces.rest.resources.WarehouseResource;
+
+/**
+ * Assembler class to create a WarehouseResource from a Warehouse entity.
+ * @since 1.0.0
+ */
+public class WarehouseResourceFromEntityAssembler {
+
+    /**
+     * Converts a Warehouse entity to a WarehouseResource.
+     *
+     * @param entity the Warehouse entity to convert
+     * @return a WarehouseResource created from the entity
+     */
+    public static WarehouseResource toResourceFromEntity(Warehouse entity) {
+        return new WarehouseResource(entity.getName(),
+                                     entity.getAddress(),
+                                     entity.getTemperature(),
+                                     entity.getCapacity(),
+                                     entity.getImageUrl());
+    }
+}
