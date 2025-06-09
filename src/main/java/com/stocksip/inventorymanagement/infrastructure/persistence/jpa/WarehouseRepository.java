867bbdf877b66832a1f50322149156142a020d0f
Far14z <faridce14@gmail.com>
2025-06-09 04:10:17 -0500

    feat(warehouses): add queries for the update warehouse command.

--- src/main/java/com/stocksip/inventorymanagement/infrastructure/persistence/jpa/WarehouseRepository.java
+++ src/main/java/com/stocksip/inventorymanagement/infrastructure/persistence/jpa/WarehouseRepository.java
@@ -19,10 +19,30 @@ public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
     /**
      * Checks if a warehouse exists by its address details.
      *
-     * @param street the street of the warehouse address
-     * @param city the city of the warehouse address
+     * @param street     the street of the warehouse address
+     * @param city       the city of the warehouse address
      * @param postalCode the postal code of the warehouse address
      * @return true if a warehouse with the specified address exists, false otherwise
      */
     boolean existsByAddressStreetAndAddressCityAndAddressPostalCode(String street, String city, String postalCode);
+
+    /**
+     * Checks if a warehouse exists by its name and a different warehouse ID.
+     *
+     * @param name the name of the warehouse
+     * @param warehouseId the unique identifier of the warehouse
+     * @return true if a warehouse with the specified name exists and is not the one with the given ID, false otherwise
+     */
+    boolean existsByNameAndWarehouseIdIsNot(String name, Long warehouseId);
+
+    /**
+     * Checks if a warehouse exists by its address details, excluding a specific warehouse ID.
+     *
+     * @param street     the street of the warehouse address
+     * @param city       the city of the warehouse address
+     * @param postalCode the postal code of the warehouse address
+     * @param warehouseId         the ID of the warehouse to exclude from the check
+     * @return true if a warehouse with the specified address exists and is not the one with the given ID, false otherwise
+     */
+    boolean existsByAddressStreetIgnoreCaseAndAddressCityIgnoreCaseAndAddressPostalCodeIgnoreCaseAndWarehouseIdIsNot(String street, String city, String postalCode, Long warehouseId);
 }
