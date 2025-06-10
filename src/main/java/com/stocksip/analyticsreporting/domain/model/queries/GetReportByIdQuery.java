e2c5c183911bdb8d636afc4c82ac538aec254494
Matias Diaz <147676997+equinox-1092@users.noreply.github.com>
2025-06-09 22:43:34 -0500

    feat(queries): add query by id to report

---
+++ src/main/java/com/stocksip/analyticsreporting/domain/model/queries/GetReportByIdQuery.java
@@ -0,0 +1,14 @@
+package com.stocksip.analyticsreporting.domain.model.queries;
+
+/**
+ * @summary
+ * This class represents the query to get a report by its id.
+ * @param id - the id of the report.
+ */
+public record GetReportByIdQuery(Long id) {
+    public GetReportByIdQuery {
+        if (id == null) {
+            throw new IllegalArgumentException("Id cannot be null");
+        }
+    }
+}
