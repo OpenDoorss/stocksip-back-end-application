3945dd60b86f0b4e9238179ec5a363f4b2a5f0af
Matias Diaz <147676997+equinox-1092@users.noreply.github.com>
2025-06-09 22:42:02 -0500

    feat(queries): add by guide name

---
+++ src/main/java/com/stocksip/analyticsreporting/domain/model/queries/GetCareGuideByGuideNameQuery.java
@@ -0,0 +1,14 @@
+package com.stocksip.analyticsreporting.domain.model.queries;
+
+/**
+ * @summary
+ * This class represents the query to retrieve all reports for the guide using guideName.
+ * @param guideName - the guideName to retrieve all the data.
+ */
+public record GetCareGuideByGuideNameQuery(String guideName) {
+    public GetCareGuideByGuideNameQuery{
+        if(guideName==null || guideName.isBlank()) {
+            throw new IllegalArgumentException("Guide name cannot be null");
+        }
+    }
+}
