c378ac12af52a0b14cf1f097a08fa2dc2fa8cd87
Matias Diaz <147676997+equinox-1092@users.noreply.github.com>
2025-06-09 22:45:02 -0500

    feat(queries): add query report by date and lost amount

---
+++ src/main/java/com/stocksip/analyticsreporting/domain/model/queries/GetReportByReportDateAndLostAmountQuery.java
@@ -0,0 +1,20 @@
+package com.stocksip.analyticsreporting.domain.model.queries;
+
+import java.util.Date;
+
+/**
+ * @summary
+ * This class represents the query to get a report by reportDate and lostAmount.
+ * @param reportDate - The reportDate of the report.
+ * @param lostAmount - The lostAmount of the report.
+ */
+public record GetReportByReportDateAndLostAmountQuery(Date reportDate, double lostAmount) {
+    public GetReportByReportDateAndLostAmountQuery {
+        if (reportDate == null) {
+            throw new IllegalArgumentException("reportDate cannot be null");
+        }
+        if (lostAmount < 0) {
+            throw new IllegalArgumentException("lostAmount cannot be negative");
+        }
+    }
+}
