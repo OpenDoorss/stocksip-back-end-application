7e398d97df3558283acbb5f21e6534d18614c51b
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-17 13:05:52 -0500

    feat(products): add command service.

---
+++ src/main/java/com/stocksip/inventorymanagement/domain/services/ProductCommandService.java
@@ -0,0 +1,23 @@
+package com.stocksip.inventorymanagement.domain.services;
+
+import com.stocksip.inventorymanagement.domain.model.aggregates.Product;
+import com.stocksip.inventorymanagement.domain.model.commands.*;
+
+import java.util.Optional;
+
+/**
+ * ProductCommandService is an interface that defines methods for handling commands related to products.
+ *
+ * @summary
+ * Service interface for handling commands related to products.
+ *
+ * @since 1.0.0
+ */
+public interface ProductCommandService {
+
+    Optional<Product> handle(CreateProductCommand command, UploadImageCommand imageCommand);
+    Optional<Product> handle(UpdateProductCommand command);
+    Long handle(MoveProductToAnotherWarehouseCommand command);
+    void handle(AddStockToProductCommand command);
+    void handle(RemoveStockFromProductCommand command);
+}
