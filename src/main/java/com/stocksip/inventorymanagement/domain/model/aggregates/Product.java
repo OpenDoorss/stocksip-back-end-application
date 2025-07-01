cc20193402d3546a5cafdb09a5d37981fa42b95c
Far14z <faridce14@gmail.com>
2025-07-01 13:03:59 -0500

    fix: fix.

--- src/main/java/com/stocksip/inventorymanagement/domain/model/aggregates/Product.java
+++ src/main/java/com/stocksip/inventorymanagement/domain/model/aggregates/Product.java
@@ -14,6 +14,7 @@ import java.util.List;
 /**
  * Product Aggregate Root
  *
+ *
  * @summary
  * The Product class is an aggregate root that represents a product in a warehouse.
  * It is responsible for handling the CreateProductCommand command.
