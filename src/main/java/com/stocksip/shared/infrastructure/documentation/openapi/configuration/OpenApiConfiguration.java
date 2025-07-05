0376465b899a1dc62598567d043b30ec3adbf299
Far14z <faridce14@gmail.com>
2025-07-05 09:22:59 -0500

    feat(authentication): add jwt support for swagger.

--- src/main/java/com/stocksip/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/stocksip/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -1,5 +1,6 @@
 package com.stocksip.shared.infrastructure.documentation.openapi.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -7,6 +8,8 @@ import io.swagger.v3.oas.models.info.License;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class OpenApiConfiguration {
@@ -32,6 +35,20 @@ public class OpenApiConfiguration {
                 .externalDocs(new ExternalDocumentation()
                         .description("StockSip Platform Wiki Documentation")
                         .url("https://stocksip-platform.wiki.github.io/docs"));
+
+        // Add a security scheme
+
+        final String securitySchemeName = "bearerAuth";
+
+        openApi.addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
 
         return openApi;
     }
